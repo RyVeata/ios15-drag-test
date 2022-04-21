e679ec000e867d064dc0db1c2e33fe2df6ff1a92
Becky Veater <becky@elixel.co.uk>
2022-04-21 18:52:38 +0100

    initial build with iOS capacitor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'subpage',
+    loadChildren: () => import('./subpage/subpage.module').then( m => m.SubpagePageModule)
+  },
+  {
+    path: 'subpage2',
+    loadChildren: () => import('./subpage2/subpage2.module').then( m => m.Subpage2PageModule)
   }
 ];
 @NgModule({
